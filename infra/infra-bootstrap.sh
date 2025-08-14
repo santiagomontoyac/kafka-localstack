1a1a0b48ec01585227ca0b604c0272f89d7f467e
santiagomontoyac <santiagomontoyac@gmail.com>
2025-08-14 15:16:53 +1000

    Update infra-bootstrap.sh

--- infra/infra-bootstrap.sh
+++ infra/infra-bootstrap.sh
@@ -4,6 +4,6 @@ echo "🚀 Deploying stack to LocalStack..."
 aws --endpoint-url=http://localhost:4566 --profile localstack \
     cloudformation create-stack \
     --stack-name event-driven-app \
-    --template-body file://infra/stack.yaml
+    --template-body file://stack.yaml
 
 echo "✅ Stack deployed!"
