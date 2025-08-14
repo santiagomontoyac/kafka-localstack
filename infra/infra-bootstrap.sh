cbdbd261908c15ff513c5bdd9bf8c94adbbd5d00
santiagomontoyac <santiagomontoyac@gmail.com>
2025-08-14 15:30:36 +1000

    Update infra-bootstrap.sh

--- infra/infra-bootstrap.sh
+++ infra/infra-bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 echo "🚀 Deploying stack to LocalStack..."
 
 aws --endpoint-url=http://localhost:4566 \
+    --region us-east-1 \
     cloudformation create-stack \
-    --stack-name event-driven-app \
+    --stack-name kafka-stack \
     --template-body file://stack.yaml
 
 echo "✅ Stack deployed!"
