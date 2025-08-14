82e5fb0878b2c038bb482aabc75d20cbef6973cc
santiagomontoyac <santiagomontoyac@gmail.com>
2025-08-14 15:34:02 +1000

    Update infra-bootstrap.sh

--- infra/infra-bootstrap.sh
+++ infra/infra-bootstrap.sh
@@ -2,10 +2,14 @@
 
 echo "🚀 Deploying stack to LocalStack..."
 
+# Export dummy credentials for LocalStack
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+
 aws --endpoint-url=http://localhost:4566 \
-    --region us-east-1 \
     cloudformation create-stack \
     --stack-name kafka-stack \
-    --template-body file://stack.yaml
+    --template-body file://infra/stack.yaml
 
 echo "✅ Stack deployed!"
